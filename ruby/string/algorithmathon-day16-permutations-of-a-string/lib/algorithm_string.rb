3c0a06fb10e979196b93a31140854f79f6d54674
Chris Lynch <chrislynch42@yahoo.com>
2013-08-09 14:37:37 -0400

    Correct Solution but is not the solution I wanted.

--- ruby/string/algorithmathon-day16-permutations-of-a-string/lib/algorithm_string.rb
+++ ruby/string/algorithmathon-day16-permutations-of-a-string/lib/algorithm_string.rb
@@ -4,34 +4,43 @@ class AlgorithmString < String
   #Determine all possible orderings of characters in a string
   #
   #  - returns an array with all the possible orderings.
-  def permutate()   
+  def permutate()
+    @nomore=0
     return_array = Array.new()
-    permutate_by_recursion(return_array,self,0,0,self.length-1)    
+    permutate_by_recursion(return_array,Array.new(),Array.new(),0)
     puts return_array
     return_array
   end
 
   private
-    def permutate_by_recursion(return_array, target, current_position, start_position,end_position)
-      print "current_position="
-      puts current_position
-      if current_position >= end_position
-        return
-      end
-      for i in start_position..end_position
-        switch_characters(target,current_position,i)        
-        puts target
-        return_array[return_array.length]=target[0..end_position]
-        permutate_by_recursion(return_array, target, current_position + 1, current_position + 1, end_position)
-        switch_characters(target,current_position,i)
+  def permutate_by_recursion(return_array,build_array,tracking_array, current_position)
+    final_position= self.length - 1
+
+    if current_position > final_position
+      puts 'end'
+      puts build_array.join()
+      return_array[return_array.length]=build_array.join()
+      return
+    end
+    
+    for i in 0..(self.length-1)
+      puts "i=#{i}"
+      @nomore = @nomore + 1
+      puts "nomore=#{@nomore}"
+      unless tracking_array[i]
+        build_array.push(self[i])
+        puts build_array.join()
+        tracking_array[i]=true
+        permutate_by_recursion(return_array,build_array,tracking_array,current_position + 1)
+        build_array.pop()
+        tracking_array[i]=false
       end
     end
+  end
 
-    def switch_characters(target,position_one,position_two)
-      character_holder=target[position_one]
-      target[position_one]=target[position_two]
-      target[position_two]=character_holder
-    end
-
-
+  def switch_characters(string_array,position_one,position_two)
+    character_holder=string_array[position_one]
+    string_array[position_one]=string_array[position_two]
+    string_array[position_two]=character_holder
+  end
 end
