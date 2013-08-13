b3438f01c6e806c2167fd9e7b2484ccf9c7f0a0b
Chris Lynch <chrislynch42@yahoo.com>
2013-08-13 14:16:44 -0400

    Added another solution and more rigoruos training.

--- ruby/string/algorithmathon-day16-permutations-of-a-string/lib/algorithm_string.rb
+++ ruby/string/algorithmathon-day16-permutations-of-a-string/lib/algorithm_string.rb
@@ -1,24 +1,39 @@
 class AlgorithmString < String
-
+  attr_reader :iteration_count
 
   #Determine all possible orderings of characters in a string
   #
   #  - returns an array with all the possible orderings.
   def permutate()
+    @iteration_count=0
     return_array = Array.new()
     permutate_by_recursion(return_array,Array.new(),Array.new(),0)
     return_array
   end
 
   def permutate2()
-    puts 'begin'
-    @nomore=0
+    @iteration_count=0    
     return_array = Array.new()
     string_array = self.split('')
     permutate_by_recursion2(return_array,string_array,0)
-    puts return_array
     return_array
   end
+
+  def permutate3()
+    @iteration_count=0    
+    return_array = Array.new()
+    string_array = self.split('')
+    permutate_by_recursion3(return_array,string_array,"")
+    return_array
+  end  
+
+  def permutate4()
+    @iteration_count=0    
+    return_array = Array.new()
+    string_array = self.split('')
+    permutate_by_recursion4(return_array,string_array,"",0)
+    return_array
+  end  
 
   private
   def permutate_by_recursion(return_array,build_array,tracking_array, current_position)
@@ -30,6 +45,7 @@ class AlgorithmString < String
     end
     
     for i in 0..final_position
+      @iteration_count = @iteration_count + 1
       unless tracking_array[i]
         build_array.push(self[i])
         tracking_array[i]=true
@@ -49,12 +65,10 @@ class AlgorithmString < String
     end
     
     for i in current_position..final_position
-       @nomore = @nomore + 1
-       puts @nomore
-        puts string_array.join()
-        switch_character(string_array,current_position,i)
-        permutate_by_recursion2(return_array,string_array,current_position + 1)
-        switch_character(string_array,current_position,i)
+      @iteration_count = @iteration_count + 1
+      switch_character(string_array,current_position,i)
+      permutate_by_recursion2(return_array,string_array,current_position + 1)
+      switch_character(string_array,current_position,i)
     end
   end  
 
@@ -63,4 +77,35 @@ class AlgorithmString < String
     string_array[second]=string_array[first]
     string_array[first]=value_holder
   end
+
+  def permutate_by_recursion3(return_array, string_array, string_value)
+    if string_array.nil?  || string_array.length < 1
+      return_array[return_array.length]=string_value
+      return
+    end
+    string_array.each_with_index { | value, index |
+      @iteration_count = @iteration_count + 1
+      prefix = string_value + value
+      temp_array = string_array.clone()
+      temp_array.delete_at(index)
+      permutate_by_recursion3(return_array, temp_array, prefix)
+    }
+
+  end 
+
+  def permutate_by_recursion4(return_array, string_array, string_value,current_level)
+    if string_array.nil?  || string_array.length < 1
+      return_array[return_array.length]=string_value
+      return
+    end
+    string_array.each_with_index { | value, index |
+      @iteration_count = @iteration_count + 1
+      prefix = string_value + value
+      temp_array = string_array.clone()
+      temp_array.delete_at(index)
+      permutate_by_recursion4(return_array, temp_array, prefix, current_level + 1)
+    }
+
+  end  
+  
 end
--- ruby/string/algorithmathon-day16-permutations-of-a-string/spec/algorithm_string_spec.rb
+++ ruby/string/algorithmathon-day16-permutations-of-a-string/spec/algorithm_string_spec.rb
@@ -6,6 +6,10 @@ describe AlgorithmString do
     it "should have method 'permutate'" do
       AlgorithmString.method_defined?(:permutate).should be_true
     end
+
+    it "should have method 'iteration_count'" do
+      AlgorithmString.method_defined?(:iteration_count).should be_true
+    end    
   end
 
   describe "Instance" do
@@ -13,20 +17,124 @@ describe AlgorithmString do
     before(:each) do
     end
 
-    it "should not return nil when 'permutate()' is called" do
-      @string = AlgorithmString.new('ABC')
-      @string.permutate().should_not be_nil
-    end
+    describe "'ABC' permutations" do
+      before(:each) do
+        @string = AlgorithmString.new('ABC')
+      end
 
-    it "should return 6 when 'permutate()' is called" do
-      @string = AlgorithmString.new('ABC')
-      @string.permutate().length.should == 6
-    end 
-  
-    it "should return 24 when 'permutate()' is called" do
-      @string = AlgorithmString.new('ABCD')
-      @string.permutate2().length.should == 24
-    end   
+      describe "When 'permutate()' is called" do
+        it "should not return nil" do
+          @string.permutate().should_not be_nil
+        end
+
+        it "should return 6" do
+          @string.permutate().length.should == 6
+        end 
+
+        it "should return 6 when 'iteration_count' is called" do
+          @string.permutate()
+          @string.iteration_count.should == 6
+        end 
+
+        it "should match @verify_array" do
+          verify_array = "ABC".chars.to_a.permutation.map(&:join)
+          local_array = @string.permutate()
+          verify_array.each { | value |
+            local_array.include?(value).should be_true
+          }
+        end
+      end
+
+      describe "When 'permutate2()' is called" do
+        it "should not return nil" do
+          @string.permutate2().should_not be_nil
+        end
+
+        it "should return 6" do
+          @string.permutate2().length.should == 6
+        end 
+
+        it "should return 6 when 'iteration_count' is called" do
+          @string.permutate2()
+          @string.iteration_count.should == 6
+        end 
+
+        it "should match @verify_array" do
+          verify_array = "ABC".chars.to_a.permutation.map(&:join)
+          local_array = @string.permutate2()
+          verify_array.each { | value |
+            local_array.include?(value).should be_true
+          }
+        end
+      end
+
+      describe "When 'permutate3()' is called" do
+        it "should not return nil" do
+          @string.permutate3().should_not be_nil
+        end
+
+        it "should return 6" do
+          @string.permutate3().length.should == 6
+        end 
+
+        it "should return 6 when 'iteration_count' is called" do
+          @string.permutate3()
+          @string.iteration_count.should == 6
+        end 
+
+        it "should match @verify_array" do
+          verify_array = "ABC".chars.to_a.permutation.map(&:join)
+          local_array = @string.permutate3()
+          verify_array.each { | value |
+            local_array.include?(value).should be_true
+          }
+        end
+      end
+
+      describe "When 'permutate4()' is called" do
+        it "should not return nil" do
+          @string.permutate4().should_not be_nil
+        end
+
+        it "should return 6" do
+          @string.permutate4().length.should == 6
+        end 
+
+        it "should return 6 when 'iteration_count' is called" do
+          @string.permutate4()
+          @string.iteration_count.should == 6
+        end 
+
+        it "should match @verify_array" do
+          verify_array = "ABC".chars.to_a.permutation.map(&:join)
+          local_array = @string.permutate4()
+          verify_array.each { | value |
+            local_array.include?(value).should be_true
+          }
+        end
+      end
+
+    end
+    describe "'ABCD' permutations" do
+      before(:each) do
+        @string = AlgorithmString.new('ABCD')
+      end
+      it "should return 24 when 'permutate()' is called" do
+        @string.permutate().length.should == 24
+      end   
+      it "should return 24 when 'permutate()' is called" do
+        @string.permutate()
+        @string.iteration_count.should == 24
+      end
+
+      it "should return 24 when 'permutate2()' is called" do
+        @string.permutate2().length.should == 24
+      end   
+      it "should return 24 when 'permutate2()' is called" do
+        @string.permutate2()
+        @string.iteration_count.should == 24
+      end            
+    end
   end
 
 end
