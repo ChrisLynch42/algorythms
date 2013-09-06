8f753d1a5680e3b6d990f6dd96e83997cfa725e6
Chris Lynch <chrislynch42@yahoo.com>
2013-09-06 13:37:55 -0400

    First solution was incorrect. This second solution is correct

--- ruby/array/algorithmathon-day19-combinations/lib/algorithm_array.rb
+++ ruby/array/algorithmathon-day19-combinations/lib/algorithm_array.rb
@@ -1,55 +1,41 @@
 class AlgorithmArray < Array
-  #Find the location of the matching element in the array.
-  #
-  #* *Args*    :
-  #+search_object+:: The object being looked for.
-  #* *Returns* :
-  #  - an integer representing the location in the array the matching element is located.             
+  attr_reader :iteration_count
+
+
   def combinations(combination_length)
+    @iteration_count=0
+    @level=0
     unless combination_length.nil? || combination_length < 1
+      positions = combination_length - 1
       return_array = Array.new()
-      combinations_by_recursion(return_array,combination_length-2,0,combination_length-1)
+      build_array = self[0..positions]
+      return_array[return_array.length]= build_array.join()
+      build_array.pop()
+      positions.downto(0) do | position |
+        combinate_by_recursion(return_array,build_array,positions,position,position+1)
+        @level = @level - 1
+        build_array.pop()
+      end
       return_array
     end
   end
 
-
-  #Find the location of the matching element in the array.
-  #
-  #* *Args*    :
-  #+search_object+:: The object being looked for.
-  #+search_location+:: The location in the array to compare values.
-  #+high+:: Tracks the lowest location in the array that is know to contain a value greater than the search_object.
-  #+high+:: Tracks the highest location in the array that is know to contain a value less than the search_object.
-  #* *Returns* :
-  #  - an integer representing the location in the array the matching element is located or nil if it is not found.               
-  def combinations_by_recursion(return_array,prefix_length,start_position,end_position)
-    if start_position > (self.length - 1) 
-
+  def combinate_by_recursion(return_array,build_array,positions,current_position,end_position)
+    @level = @level + 1
+    if positions < current_position
+      return_array[return_array.length]= build_array.join()
       return
     end
-    current_position=start_position
-    current_array = Array.new()
-    for current_count in 0..prefix_length
-      current_array[current_count]=self[current_position]
-      current_position=get_next_position(current_position)
-    end
-    current_array[current_array.length]=self[end_position]
-    return_array[return_array.length]=current_array.to_s
-    end_position=get_next_position(end_position)
-    if (start_position - 1) <= end_position
-      start_position=start_position + 1
-      end_position=get_next_position(start_position + prefix_length)
-    end
-    combinations_by_recursion(return_array,prefix_length,start_position,end_position)
-  end
 
-  def get_next_position(current_position)
-    if current_position >= (self.length - 1)
-      current_position = 0
-    else
-      current_position = current_position + 1
-    end    
-  end
+    while end_position < self.length
+      @iteration_count = @iteration_count + 1
+      build_array.push(self[end_position])
+      combinate_by_recursion(return_array,build_array,positions,current_position + 1,end_position + 1)
+      @level = @level - 1
+      build_array.pop()
+      end_position=end_position+1
+    end
+  end  
+
 
 end
--- ruby/array/algorithmathon-day19-combinations/spec/algorithm_array_spec.rb
+++ ruby/array/algorithmathon-day19-combinations/spec/algorithm_array_spec.rb
@@ -7,33 +7,30 @@ describe AlgorithmArray do
       AlgorithmArray.method_defined?(:combinations).should be_true
     end
 
-    it "should have method 'get_next_position'" do
-      AlgorithmArray.method_defined?(:get_next_position).should be_true
-    end
   end
 
   describe "Instance" do
     before(:each) do
     end
 
-    it "should return 2 when 'get_next_position' is called" do
-      @array = AlgorithmArray.[](1,2,3)
-      @array.get_next_position(1).should == 2
-    end
-
-    it "should return 0 when 'get_next_position' is called" do
-      @array = AlgorithmArray.[](1,2,3)
-      @array.get_next_position(2).should == 0
-    end
-
-    it "should return 3 when 'combinations(2).length' is called" do
+    it "should return 3 when '[].combinations(2).length' is called" do
       @array = AlgorithmArray.[](1,2,3)
       @array.combinations(2).length.should == 3
     end
 
-    it "should return 9 when 'combinations(3).length' is called" do
+    it "should return 10 when '[].combinations(3).length' is called" do
       @array = AlgorithmArray.[](1,2,3,4,5)
-      @array.combinations(3).length.should == 9
+      @array.combinations(3).length.should == 10
+    end
+
+    it "should return 20 when '[].combinations(3).length' is called" do
+      @array = AlgorithmArray.[](1,2,3,4,5,6)
+      @array.combinations(3).length.should == 20
+    end
+
+    it "should return 15 when '[1,2,3,4,5,6].combinations(4).length' is called" do
+      @array = AlgorithmArray.[](1,2,3,4,5,6)
+      @array.combinations(4).length.should == 15
     end
 
   end
