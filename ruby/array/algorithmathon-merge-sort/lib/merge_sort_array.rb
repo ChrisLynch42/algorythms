2f80a7470ae01816b13e928dc9e5488ae7ff5ca1
Chris Lynch <chrislynch42@yahoo.com>
2014-11-24 19:41:15 -0500

    A working merge sort with recursion

--- ruby/array/algorithmathon-merge-sort/lib/merge_sort_array.rb
+++ ruby/array/algorithmathon-merge-sort/lib/merge_sort_array.rb
@@ -17,26 +17,17 @@ class MergeSortArray < Array
   #* *Returns* :
   #  - this array.
   def merge_sort()
-
+    break_array_apart_then_merge(self)
   end
 
   def break_array_apart_then_merge(local_array)
+    return_array = local_array
     if local_array.length > 1
-      split_index = local_array / 2
-      break_array_apart_then_merge(local_array.shift(split_index))
-      break_array_apart_then_merge(local_array)
+      split_index = local_array.length / 2
+      array_of_arrays = local_array.each_slice(split_index).to_a
+      return_array = merge_arrays(break_array_apart_then_merge(array_of_arrays[0]), break_array_apart_then_merge(array_of_arrays[1]))
     end
-  end
-
-
-
-
-  def sort_arrays_by_size_then_merge(array_one, array_two)
-    if array_one.length > array_two.length
-      merge_arrays(array_one,array_two)
-    else
-      merge_arrays(array_two,array_one)
-    end
+    return_array
   end
 
   def merge_arrays(array_one, array_two)
@@ -44,8 +35,6 @@ class MergeSortArray < Array
     index = 0
     while array_one.length > 0 || array_two.length > 0
       if array_one.length > 0 && array_two.length > 0
-        puts "array one #{array_one[index]}"
-        puts "array two #{array_two[index]}"
         if array_one[index] > array_two[index]
           add_to_array(return_array, array_two.shift,array_one.shift)
         else
@@ -53,11 +42,9 @@ class MergeSortArray < Array
         end
       else
         if array_one.length > 0
-          puts "array one #{array_one[index]}"
           add_array_to_array(return_array,array_one)
         end
         if array_two.length > 0
-          puts "array two #{array_one[index]}"
           add_array_to_array(return_array,array_two)
         end
       end
@@ -68,7 +55,6 @@ class MergeSortArray < Array
   def add_to_array(target_array,value_one, value_two)
     target_array[target_array.length] = value_one
     target_array[target_array.length] = value_two
-    puts "target_array #{target_array.to_s}"
   end
 
   def add_array_to_array(target_array,source_array)
--- ruby/array/algorithmathon-merge-sort/spec/merge_sort_array_spec.rb
+++ ruby/array/algorithmathon-merge-sort/spec/merge_sort_array_spec.rb
@@ -58,25 +58,25 @@ describe MergeSortArray do
         @array = MergeSortArray.[](1,31,7,23,13,17,19,11,29,3)
       end
 
-      # it "should return 2 when 'new_find(7)' is called" do
-      #   expect(@array.new_find(7)).to equal(2)
-      # end
-      #
-      # it "should return 0 when 'new_find(1)' is called" do
-      #   expect(@array.new_find(1)).to equal(0)
-      # end
-      #
-      # it "should return 8 when 'new_find(29)' is called" do
-      #   expect(@array.new_find(29)).to equal(8)
-      # end
-      #
-      # it "should return 4 when 'new_find(13)' is called" do
-      #   expect(@array.new_find(13)).to equal(4)
-      # end
-      #
-      # it "should return nil when 'new_find(443)' is called" do
-      #   expect(@array.new_find(443)).to equal(nil)
-      # end
+      it "should return 2 when 'new_find(7)' is called" do
+        expect(@array.new_find(7)).to equal(2)
+      end
+
+      it "should return 0 when 'new_find(1)' is called" do
+        expect(@array.new_find(1)).to equal(0)
+      end
+
+      it "should return 8 when 'new_find(29)' is called" do
+        expect(@array.new_find(29)).to equal(8)
+      end
+
+      it "should return 4 when 'new_find(13)' is called" do
+        expect(@array.new_find(13)).to equal(4)
+      end
+
+      it "should return nil when 'new_find(443)' is called" do
+        expect(@array.new_find(443)).to equal(nil)
+      end
     end
 
   end
